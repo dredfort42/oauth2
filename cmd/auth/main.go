63a6ee8027cac8ab60d142b8bbf488d1ee2b52de
Dmitry Novikov <dredfort.42@gmail.com>
2024-06-30 22:11:32 +0100

    database connection config check added

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"auth/internal/api"
 	"auth/internal/db"
+	"os"
 
 	cfg "github.com/dredfort42/tools/configreader"
+	loger "github.com/dredfort42/tools/logprinter"
 )
 
 func main() {
 	err := cfg.GetConfig()
 	if err != nil {
 		panic(err)
+	}
+
+	if os.Getenv("DEBUG") == "1" {
+		for key, value := range cfg.Config {
+			if key == "db.password" {
+				value = "********"
+			}
+
+			loger.Debug(key, value)
+		}
 	}
 
 	db.DatabaseInit()
--- internal/db/database_connect.go
+++ internal/db/database_connect.go
@@ -2,31 +2,54 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 
 	cfg "github.com/dredfort42/tools/configreader"
 )
 
 // databaseConnect connects to the database
 func databaseConnect() {
-	var url string = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Config["db.host"],
-		cfg.Config["db.port"],
-		cfg.Config["db.user"],
-		cfg.Config["db.password"],
-		cfg.Config["db.database.name"],
-		cfg.Config["db.security.ssl"])
+	host := cfg.Config["db.host"]
+	if host == "" {
+		panic("Database host is not set")
+	}
+
+	port := cfg.Config["db.port"]
+	if port == "" {
+		panic("Database port is not set")
+	}
+
+	user := cfg.Config["db.user"]
+	if user == "" {
+		panic("Database user is not set")
+	}
+
+	password := cfg.Config["db.password"]
+	if password == "" {
+		panic("Database password is not set")
+	}
+
+	databaseName := cfg.Config["db.database.name"]
+	if databaseName == "" {
+		panic("Database name is not set")
+	}
+
+	ssl := cfg.Config["db.security.ssl"]
+	if ssl == "" {
+		panic("Database ssl is not set")
+	}
+
+	url := "host=" + host + " port=" + port + " user=" + user + " password=" + password + " dbname=" + databaseName + " sslmode=" + ssl
 
 	var err error
 	db.database, err = sql.Open("postgres", url)
 	if err != nil {
 		db.database.Close()
-		panic(err)
+		panic("Database connection error | " + err.Error())
 	}
 
 	err = db.database.Ping()
 	if err != nil {
 		db.database.Close()
-		panic(err)
+		panic("Database ping error | " + err.Error())
 	}
 }
